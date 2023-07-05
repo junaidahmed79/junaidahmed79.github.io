343e16561113d0c624eb30ed4b23d295dfab2578
Junaid Ahmed79 <junaidahmed5760@gmail.com>
2023-07-06 00:23:48 +0100

    set the description for projects

--- js/custom/projects.js
+++ js/custom/projects.js
@@ -2,11 +2,9 @@ let projects = [
     {
         "id": 1,
         "type": "Web",
-        "title": "Home page for Brand elite",
+        "title": " Brand elite Web Application",
         "desc": `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt dolor vitae vestibulum ultrices. Suspendisse scelerisque, lectus id gravida venenatis, lacus ligula dictum purus, vitae pharetra metus lorem scelerisque urna. Quisque venenatis erat erat, eget dapibus ipsum aliquet vitae. Sed gravida tincidunt tempor.
-        <br><br>
-        In mauris enim, pulvinar ut venenatis in, dictum quis nulla. Cras imperdiet blandit eros quis consectetur. Quisque fringilla feugiat consectetur.
+        Brand elite provides a services to others for the Ecommerece business such AS Amazon , ebay and walmart etc. It offers consultancy and help to build the business and provide solution for Account management , Listing optimization , Advertising like PPC and more.
         `,
         "img": "images/projects/web/check.png",
         "url": ""
@@ -14,13 +12,11 @@ let projects = [
     {
         "id": 2,
         "type": "Web",
-        "title": "Chat App",
+        "title": "Landing Page of Bingtech Solutions",
         "desc": `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt dolor vitae vestibulum ultrices. Suspendisse scelerisque, lectus id gravida venenatis, lacus ligula dictum purus, vitae pharetra metus lorem scelerisque urna. Quisque venenatis erat erat, eget dapibus ipsum aliquet vitae. Sed gravida tincidunt tempor.
-        <br><br>
-        In mauris enim, pulvinar ut venenatis in, dictum quis nulla. Cras imperdiet blandit eros quis consectetur. Quisque fringilla feugiat consectetur.
+        Bingtechs Solutions is software agency which provides services to others for Graphic Designing , Web development , App development , Creative Degign and Email Marketing.
         `,
-        "img": "images/projects/web/chat_app.png",
+        "img": "images/projects/web/bing check.png",
         "url": ""
     },
     {
