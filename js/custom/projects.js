f173e5e7383f1b910e89349f8f1a4eb2df0ce0da
Junaid Ahmed79 <junaidahmed5760@gmail.com>
2023-08-03 23:28:57 +0100

    pro change

--- js/custom/projects.js
+++ js/custom/projects.js
@@ -1,41 +1,63 @@
 let projects = [
-    {
-        "id": 1,
-        "type": "Web",
-        "title": " Brand elite Web Application",
-        "desc": `
-        Brand elite provides a services to others for the Ecommerece business such AS Amazon , ebay and walmart etc. It offers consultancy and help to build the business and provide solution for Account management , Listing optimization , Advertising like PPC and more.
-        `,
-        "img": "images/projects/web/check.png",
-        "url": ""
-    },
-    {
-        "id": 2,
-        "type": "Web",
-        "title": " forecaster for fuel costs",
-        "desc": `
-        
-        `,
-        "img": "images/projects/web/forcaster.png",
-        "url": ""
-    },
-    {
-        "id": 3,
-        "type": "Web",
-        "title": "Landing Page of Bingtech Solutions",
-        "desc": `
-        Bingtechs Solutions is software agency which provides services to others for Graphic Designing , Web development , App development , Creative Degign and Email Marketing.
-        `,
-        "img": "images/projects/web/bingtech.png",
-        "url": ""
-    },
-    {
-        "id": 4,
-        "type": "Design",
-        "title": "Colorization",
-        "desc": ``,
-        "img": "images/projects/design/color.jpeg",
-        "url": ""
-    },
-    
+{
+"id": 1,
+"type": "Web",
+"title": " Password Generator",
+"desc": `
+The Simple Password Generator is a user-friendly web-based application designed to create passwords quickly and easily. With this tool, users can generate passwords of varying strengths to enhance their online security.<br><br>
+This straightforward app provides four distinct options for password generation: "Super Strong," "Strong," "Weak," and "Funny." Users can select their preferred strength level based on their specific requirements and preferences.
+`,
+"img": "images/projects/web/pw_gen.png",
+"url": ""
+},
+{
+"id": 2,
+"type": "Web",
+"title": " Brand elite Web Application",
+"desc": `
+Brand elite provides a services to others for the Ecommerece business such AS Amazon , ebay and walmart etc. It offers consultancy and help to build the business and provide solution for Account management , Listing optimization , Advertising like PPC and more.
+`,
+"img": "images/projects/web/check.png",
+"url": ""
+},
+{
+"id": 3,
+"type": "Web",
+"title": "Landing Page of Bingtech Solutions",
+"desc": `
+Bingtechs Solutions is software agency which provides services to others for Graphic Designing , Web development , App development , Creative Degign and Email Marketing.
+`,
+"img": "images/projects/web/bingtech.png",
+"url": ""
+},
+{
+"id": 4,
+"type": "Web",
+"title": " forecaster for fuel costs",
+"desc": `
+
+`,
+"img": "images/projects/web/forcaster.png",
+"url": ""
+},
+{
+"id": 5,
+"type": "Web",
+"title": " Login page for Airline system",
+"desc": `
+
+`,
+"img": "images/projects/web/login.png",
+"url": ""
+},
+
+{
+"id": 6,
+"type": "Design",
+"title": "Colorization",
+"desc": ``,
+"img": "images/projects/design/color.jpeg",
+"url": ""
+},
+
 ]
